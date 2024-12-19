d04667cb6a1f0fccdb4da215dd25853a30b67f53
FallenNinja2 <jerryjr.casem@sdca.edu.ph>
2024-12-19 13:11:00 +0800

    Add files via upload

---
+++ ecommerce/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset="UTF-5">
+    <meta name="viewport" content="width=device=width, initial=scale=1.0">
+    <title>eCommerce Home Page</title>
+    
+    <style>
+        body, html {
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .content {
+            flex: 1;
+        }
+
+        footer {
+            background-color: aqua;
+            color: white;
+            padding: 15px;
+        }
+        </style>
+</head>
+</body>
